28c57c02d5df70775ce63e6172c2112008e602af
Akash <abirakash1987@gmail.com>
2025-07-04 00:13:49 +0600

    added vercel.json

--- index.js
+++ index.js
@@ -4,20 +4,6 @@ const port = 8080;
 const app = express();
 
 
-app.get('/' , (req , res) => {
-    res.json({
-        location : 'home',
-        path : __dirname
-    });
-})
-
-app.get('/download' , (req , res) => {
-    res.json({
-        location : 'download',
-        path : __dirname
-    });
-})
-
 app.listen(port , () => {
     console.log(`App is listening at port: ${port}`);
 })
